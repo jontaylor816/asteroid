e801e95c46b2855d2aaa2b45ec87fafb02fdbe8f
Jonathan Taylor <jontaylor816@gmail.com>
2025-09-22 18:32:07 -0500

    asteroids split

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
 from circleshape import CircleShape
+import random
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        split_angle = random.uniform(20, 50)
+
+        split1 = self.velocity.rotate(split_angle)
+        split2 = self.velocity.rotate(-split_angle)
+        new_rad = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, new_rad)
+        asteroid.velocity = split1 * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_rad)
+        asteroid.velocity = split2 * 1.2
--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
                 sys.exit()
             for shot in shots:
                 if asteroid.collision_check(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill("black")
